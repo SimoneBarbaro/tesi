01e21b2f9ff08fcea56a2181a13e670d09f72b72
Simone <simone.barbaro2@studio.unibo.it>
2019-02-15 18:21:55 +0100

    corrected bug

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from google.colab import drive
-import json
 from models import ModelType
 import experiment
 
 drive.mount('/content/drive/')
 with open('/content/drive/My Drive/Colab Notebooks/data/inception', 'w') as file:
-    result = experiment.experiment_model(ModelType.INCEPTION_V3)
-    result = {'inception_results': result}
-    file.write(json.dumps(result))
+    file.write(str(experiment.experiment_model(ModelType.INCEPTION_V3)))
 with open('/content/drive/My Drive/Colab Notebooks/data/resnet', 'w') as file:
-    result = experiment.experiment_model(ModelType.RES_NET_50)
-    result = {'resnet_results': result}
-    file.write(json.dumps(result))
+    file.write(str(experiment.experiment_model(ModelType.RES_NET_50)))
 with open('/content/drive/My Drive/Colab Notebooks/data/densenet', 'w') as file:
-    result = experiment.experiment_model(ModelType.DENSE_NET_121)
-    result = {'densenet_results': result}
-    file.write(json.dumps(result))
+    file.write(str(experiment.experiment_model(ModelType.DENSE_NET_121)))
 
