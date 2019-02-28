e0c673454fedf23e39fc3898884597d8c36839de
Simone <simone.barbaro2@studio.unibo.it>
2019-02-28 14:42:53 +0100

    added confing file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
+import json
 from data import Dataset
 from experiment import Experiment, ExperimentState
 
+with open('confing.json') as f:
+    config_data = json.load(f)
+
 dataset = Dataset()
 num_lines = sum(1 for line in open(sys.argv[2]))
-s = ExperimentState([1, 3], "batch_size")
+s = ExperimentState(config_data["batch_sizes"], "batch_size")
 with open(sys.argv[2], 'a') as file:
-    Experiment(sys.argv[1], dataset, [1], file, s, ['accuracy']).resume()
+    Experiment(sys.argv[1], dataset, config_data["epochs"], file, s, ['accuracy']).resume()
