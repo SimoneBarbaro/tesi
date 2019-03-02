b2034867f787bc5b8fa352fa738b58da9414d0eb
Simone <simone.barbaro2@studio.unibo.it>
2019-03-02 14:18:29 +0100

    changed arg gestion

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ import json
 from data import Dataset
 from experiment import Experiment, ExperimentState
 
-with open('confing.json') as f:
-    config_data = json.load(f)
+if __name__ == "__main__":
+    with open('confing.json') as f:
+        config_data = json.load(f)
+    log_dir = None
+    if len(sys.argv) > 1:
+        model = sys.argv[1]
+    else:
+        model = "resnet"
+    if len(sys.argv) > 2:
+        file = open(sys.argv[2])
+    else:
+        file = sys.stdin
+    if len(sys.argv) > 3:
+        log_dir = sys.argv[3]
 
-dataset = Dataset()
-num_lines = sum(1 for line in open(sys.argv[2]))
-s = ExperimentState(config_data["batch_sizes"], "batch_size")
-with open(sys.argv[2], 'a') as file:
-    Experiment(sys.argv[1], dataset, config_data["epochs"], file, s, ['accuracy']).resume()
+    dataset = Dataset()
+    s = ExperimentState(config_data["batch_sizes"], "batch_size")
+    Experiment(model, dataset, config_data["epochs"], file, s, ['accuracy'], log_dir).resume()
+
+    if file is not sys.stdin:
+        file.close()
