50c972726b779cd52e27754d6b5f2af4ddf8f32c
Simone <simone.barbaro2@studio.unibo.it>
2019-03-02 12:05:46 +0100

    test

--- execution.py
+++ execution.py
@@ -12,7 +12,7 @@ class Execution:
                              epochs=self.epochs,
                              validation_split=0.0,
                              callbacks=callbacks,
-                             verbose=0)
+                             verbose=1)
 
     def evaluate(self):
         return self.model.model.evaluate(self.data.validation_x,
