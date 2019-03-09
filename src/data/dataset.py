19b0a6eda204162f3511d2eb7c055f8c138dd271
Simone <simone.barbaro2@studio.unibo.it>
2019-03-09 10:11:03 +0100

    added image size to data description

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -6,8 +6,6 @@ from scipy import ndimage
 class Dataset:
 
     def __init__(self, mat_file):
-        self.images_width = 64
-        self.input_shape = (self.images_width, self.images_width, 3)
         mat = scipy.io.loadmat(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, mat_file))
         data = mat["data"]
         self.directory = os.path.join(data[0, 0][0][0][0], data[0, 0][0][1][0])
@@ -25,3 +23,5 @@ class Dataset:
             file_path = os.path.join(self.directory, img_path[0], img_names[i][0])
             image = ndimage.imread(file_path, mode="RGB")
             self.imgs.append(image)
+        self.images_width = data[0, 7][0][0]
+        self.input_shape = (self.images_width, self.images_width, 3)
