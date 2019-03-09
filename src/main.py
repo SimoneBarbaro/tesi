5f443275103778601e4cddd1f9a7d74fc250d700
Simone <simone.barbaro2@studio.unibo.it>
2019-03-09 09:28:44 +0100

    variation of config

--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ from experiment.configs import Config
 from experiment.experiment import Experiment
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), os.pardir, 'config', 'config.json')) as f:
-        config_data = json.load(f)
     log_dir = None
     if len(sys.argv) > 1:
         model = sys.argv[1]
@@ -17,8 +15,14 @@ if __name__ == "__main__":
     else:
         file = sys.stdin
     if len(sys.argv) > 3:
-        log_dir = sys.argv[3]
+        config_file = sys.argv[3]
+    else:
+        config_file = 'eilat_config.json'
+    if len(sys.argv) > 4:
+        log_dir = sys.argv[4]
 
+    with open(os.path.join(os.path.dirname(__file__), os.pardir, 'config', config_file)) as f:
+        config_data = json.load(f)
     config = Config(config_data)
     Experiment(config, file, log_dir).resume()
 
