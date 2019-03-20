372a46d3dc7933b42810f848a2f0795d74023135
Simone <simone.barbaro2@studio.unibo.it>
2019-03-20 10:59:39 +0100

    corrected bug

--- src/data/protocol.py
+++ src/data/protocol.py
@@ -3,11 +3,10 @@ import scipy.io as sio
 
 
 class Protocol:
-    _folds = []
 
     @property
     def folds(self):
-        return self._folds
+        raise NotImplementedError
 
     def save_folds(self, file_name):
         sio.savemat(file_name, {"data": self.folds})
@@ -17,9 +16,18 @@ class PreCalculatedProtocol(Protocol):
     def __init__(self, folds):
         self._folds = folds
 
+    @property
+    def folds(self):
+        return self._folds
+
 
 class SklearnProtocol(Protocol):
     def __init__(self, num_folds, len_data, random_state=1):
         random_state = random_state
-        kfold = KFold(num_folds, True, random_state)
-        self._folds = kfold.split(range(len_data))
+        self.__kfold = KFold(num_folds, True, random_state)
+        self.__len_data = len_data
+        self._folds = self.__kfold.split(range(len_data))
+
+    @property
+    def folds(self):
+        return self.__kfold.split(range(self.__len_data))
