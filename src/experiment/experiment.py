aee3a8b4d8026062c812539f6e2d788b0d8a6c2b
Simone <simone.barbaro2@studio.unibo.it>
2019-05-02 08:53:25 +0200

    changed confusion matrix to merge fold results

--- src/experiment/experiment.py
+++ src/experiment/experiment.py
@@ -2,6 +2,9 @@ from experiment.execution import Execution
 from experiment.state import ExperimentState
 from experiment.configs import Config
 import numpy as np
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
 from tensorflow import keras
 import os
 from experiment.result_saver import ResultSaver
@@ -17,7 +20,7 @@ class CheckProgressCallback(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         epoch = epoch + 1
         if epoch in self.epochs_to_check:
-            print("epoch " + str(epoch))
+            # print("epoch " + str(epoch))
             self.evals[self.epochs_to_check.index(epoch)].append(self.evaluate())
 
 
@@ -50,6 +53,7 @@ class Experiment:
         evals = [[] for _ in range(len(config.epochs))]
         test_data = []
         f = 0
+        confusion = np.zeros((config.dataset.num_classes, config.dataset.num_classes), dtype=np.int32)
         for data in state.next_data():
             save_name = run_name + "_" + str(f) if fold_names else run_name
             f += 1
@@ -71,16 +75,11 @@ class Experiment:
                     callbacks.append(keras.callbacks.TensorBoard(log_dir=log_dir))
 
                 execution.run(callbacks)  # returns a history
+                confusion += np.array(model.confusion_matrix(data.validation_x, data.validation_y))
 
                 if save_model and self.result_saver.can_save_model():
                     model.save(self.result_saver.get_model_file(save_name), self.config.has_pretraining())
-                    model.save_confusion_matrix(data.validation_x, data.validation_y,
-                                                self.result_saver.get_confusion_file(save_name + "_validation"),
-                                                self.config.dataset.classes)
-                if save_testing:
-                    model.save_confusion_matrix(data.testing_x, data.testing_y,
-                                                self.result_saver.get_confusion_file(save_name + "_testing"),
-                                                self.config.dataset.classes)
+
             test_data.append(execution.test())
 
         dic = state.get_info().copy()
@@ -91,12 +90,38 @@ class Experiment:
             if save_output:
                 self.result_saver.write_to_output_file(dic)
 
+        self.save_confusion_matrix(confusion, self.result_saver.get_confusion_file(run_name))
+
         dic = state.get_info().copy()
         if save_testing:
             dic["epochs"] = config.epochs[-1]
             dic["data"] = "testing"
             dic["result"] = merge_results(["loss"] + config.metrics, test_data)
             self.result_saver.write_to_output_file(dic, test=True)
+
+    def save_confusion_matrix(self, matrix, file: str):
+        def insert_totals(df_cm):
+            """ insert total column and line (the last ones) """
+            sum_col = []
+            for c in df_cm.columns:
+                sum_col.append(df_cm[c].sum())
+            sum_lin = []
+            for item_line in df_cm.iterrows():
+                sum_lin.append(item_line[1].sum())
+            df_cm['total'] = sum_lin
+            sum_col.append(np.sum(sum_lin))
+            df_cm.loc['total'] = sum_col
+
+        labels = self.config.dataset.classes
+        cm = pd.DataFrame(matrix, index=labels, columns=labels)
+        insert_totals(cm)
+        ax = sn.heatmap(cm, annot=True, fmt="d")
+        ax.set_title('Confusion matrix')
+        ax.set_xlabel("Predicted")
+        ax.set_ylabel("Actual")
+
+        plt.savefig(file)
+        plt.clf()
 
 
 def merge_results(metrics, results):
